915aed3a2044f909787b0097ff4f96e49557b322
shubham <shubham.bhoyar077@gmail.com>
2023-05-01 17:55:56 +0530

    Add query for  list name and escape attempts of animals that weigh more than 10.5kg

--- queries.sql
+++ queries.sql
@@ -12,8 +12,8 @@ SELECT name from animals WHERE neutered='t' AND escape_attempts < 3;
 -- List the date of birth of all animals named either "Agumon" or "Pikachu".
 SELECT date_of_birth from animals WHERE name IN ('Agumon', 'Pikachu');
 
--- --  List name and escape attempts of animals that weigh more than 10.5kg
--- SELECT name, escape_attempts from animals WHERE weight_kg > 10.5; 
+--  List name and escape attempts of animals that weigh more than 10.5kg
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5; 
     
 -- -- Find all animals that are neutered.
 -- SELECT * from animals WHERE neutered='t';
