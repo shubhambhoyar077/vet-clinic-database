0282e682d11521a7e13817662031ef1f9cc99c00
shubham <shubham.bhoyar077@gmail.com>
2023-05-01 17:54:11 +0530

    Add query for list the name of all animals that are neutered and have less than 3 escape attempts

--- queries.sql
+++ queries.sql
@@ -5,11 +5,23 @@ SELECT * from animals WHERE name LIKE '%mon';
 
 -- List the name of all animals born between 2016 and 2019.
 SELECT * from animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
+
+-- List the name of all animals that are neutered and have less than 3 escape attempts.
+SELECT name from animals WHERE neutered='t' AND escape_attempts < 3;
+
+-- -- List the date of birth of all animals named either "Agumon" or "Pikachu".
+-- SELECT date_of_birth from animals WHERE name IN ('Agumon', 'Pikachu');
+
+-- --  List name and escape attempts of animals that weigh more than 10.5kg
+-- SELECT name, escape_attempts from animals WHERE weight_kg > 10.5; 
+    
+-- -- Find all animals that are neutered.
+-- SELECT * from animals WHERE neutered='t';
+
+-- -- Find all animals not named Gabumon.
+-- SELECT * from animals WHERE name != 'Gabumon';
+
+-- -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
+-- SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3; 
     
     
-    List the name of all animals that are neutered and have less than 3 escape attempts.
-    List the date of birth of all animals named either "Agumon" or "Pikachu".
-    List name and escape attempts of animals that weigh more than 10.5kg
-    Find all animals that are neutered.
-    Find all animals not named Gabumon.
-    Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
