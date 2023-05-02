1b7581acf502c51e6c431851ad427ed8f06f7e9a
shubham <shubham.bhoyar077@gmail.com>
2023-05-02 15:41:11 +0530

    Deleted spieces column from animals table and add species_id and owner_id as fk

--- schema.sql
+++ schema.sql
@@ -23,3 +23,16 @@ CREATE TABLE species (
     id INT GENERATED ALWAYS AS IDENTITY,
     name varchar(100),
 );
+
+ALTER TABLE animals
+DROP species;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY(species_id) 
+REFERENCES species(id)
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_owners
+FOREIGN KEY(owner_id) 
+REFERENCES owners(id)
