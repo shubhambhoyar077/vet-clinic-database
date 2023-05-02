9537e043d8f3e39fc3358b89139a340448573012
shubham <shubham.bhoyar077@gmail.com>
2023-05-02 10:05:49 +0530

    Add query for total animals

--- queries.sql
+++ queries.sql
@@ -84,3 +84,22 @@ SET weight_kg = weight_kg * -1
 WHERE weight_kg < 0;
 
 COMMIT;
+
+
+-- How many animals are there?
+SELECT COUNT(*) AS total FROM animals;
+    
+-- -- How many animals have never tried to escape?
+-- SELECT COUNT(*) AS total FROM animals WHERE escape_attempts = 0;
+
+-- -- What is the average weight of animals?
+-- SELECT AVG(weight_kg) as avg_weight FROM animals;
+
+-- -- Who escapes the most, neutered or not neutered animals?
+-- SELECT *, MAX(escape_attempts) as max_escape FROM animals GROUP BY neutered;
+
+-- -- What is the minimum and maximum weight of each type of animal?
+-- SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM animals GROUP BY species;
+
+-- -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+-- SELECT species, AVG(escape_attempts) as avg_weight FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 1990 AND 2000 GROUP BY species;
