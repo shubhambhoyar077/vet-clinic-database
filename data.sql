335d6a78610802b8ccf65b6b05a7f70da78b8e93
shubham <shubham.bhoyar077@gmail.com>
2023-05-03 13:57:15 +0530

    Insert query for vets table

--- data.sql
+++ data.sql
@@ -63,3 +63,10 @@ WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (select id from owners w
 WHEN name IN ('Angemon', 'Boarmon') THEN (select id from owners where full_name = 'Dean Winchester')
 ELSE NULL
 END;
+
+
+INSERT INTO vets (name, age, date_of_graduation)
+VALUES ('William Tatcher', 45, '2000-04-23'),
+('Maisy Smith', 26, '2019-01-17'),
+('Stephanie Mendez', 64, '1981-05-04'),
+('Jack Harkness', 38, '2008-06-08');
